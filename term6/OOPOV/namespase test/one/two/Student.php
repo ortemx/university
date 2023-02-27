83ee3deebb41ca5fb97794567c595edc0fa905e6
ortemx <printerq@yandex.ru>
2023-02-27 23:18:15 +0300

    Create Student.php

---
+++ term6/OOPOV/namespase test/one/two/Student.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace one1;
+
+class Student
+{
+    public function __construct()
+    {
+        printf("\nhello fron one\\two\\");
+    }
+}
+
+$str4 = "я строка из one\\two\\Student.php";
