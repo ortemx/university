a0a002a7524a49e47f3c3c400d9f8b68653f8339
ortemx <printerq@yandex.ru>
2023-03-04 22:47:20 +0300

    Update lab1.1.php

--- term4/php/lab1.1.php
+++ term4/php/lab1.1.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+    <title></title>
 </head>
 <body>
-	<form method="POST">
-		b<sub>1</sub>: <input type="text" name="b1"><br>
-		q: <input type="text" name="q"><br>
-		n: <input type="text" name="n"><br>
-		<input type="submit" name="button1">
-	</form>
-	<?php
-		if ($_POST['b1'] != '' && $_POST['q'] != '' && $_POST['n'] != ''){
-			if (is_numeric($_POST['b1']) && is_numeric($_POST['q']) && is_numeric($_POST['n'])){
-				$b1 = $_POST['b1'];
-				$q = $_POST['q'];
-				$n = $_POST['n'];
-				echo ($b1 * (1 - $q ** $n) / (1 - $q));		
-			} else {
-				echo ('некорректные исходные данные');
-			}
-		}
-	?>
+    <form method="POST">
+        b<sub>1</sub>: <input type="text" name="b1"><br>
+        q: <input type="text" name="q"><br>
+        n: <input type="text" name="n"><br>
+        <input type="submit" name="button1">
+    </form>
+    <?php
+    if ($_POST['b1'] != '' && $_POST['q'] != '' && $_POST['n'] != '') {
+        if (is_numeric($_POST['b1']) && is_numeric($_POST['q']) && is_numeric($_POST['n'])) {
+            $b1 = $_POST['b1'];
+            $q = $_POST['q'];
+            $n = $_POST['n'];
+            echo ($b1 * (1 - $q ** $n) / (1 - $q));
+        } else {
+            echo ('некорректные исходные данные');
+        }
+    }
+    ?>
 </body>
