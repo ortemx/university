4cd786820e9c1bae4f4219609df986dfa93d0f6f
ortemx <printerq@yandex.ru>
2023-03-04 22:47:17 +0300

    Update lab1.2.php

--- term4/php/lab1.2.php
+++ term4/php/lab1.2.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+    <title></title>
 </head>
 <body>
-	<form method="POST">
-		R: <input type="text" name="r"><br>
-		a: <input type="text" name="a"><br>
-		<input type="submit" name="button1">
-	</form>
-	<?php		
-		if ($_POST['r'] != '' && $_POST['a'] != ''){
-			if (is_numeric($_POST['r']) && is_numeric($_POST['a'])){
-				$radius = $_POST['r'];
-				$a = $_POST['a'];
-				if ($radius > $a / sqrt(3)){
-					echo ('поместится');
-				} else {
-					echo ('не поместится');
-				}
-			} else {
-				echo ('некорректные исходные данные');
-			}	
-		}
-	?>
+    <form method="POST">
+        R: <input type="text" name="r"><br>
+        a: <input type="text" name="a"><br>
+        <input type="submit" name="button1">
+    </form>
+    <?php
+    if ($_POST['r'] != '' && $_POST['a'] != '') {
+        if (is_numeric($_POST['r']) && is_numeric($_POST['a'])) {
+            $radius = $_POST['r'];
+            $a = $_POST['a'];
+            if ($radius > $a / sqrt(3)) {
+                echo ('поместится');
+            } else {
+                echo ('не поместится');
+            }
+        } else {
+            echo ('некорректные исходные данные');
+        }
+    }
+    ?>
 </body>
