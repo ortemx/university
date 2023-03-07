74572f0152354482eea0f3094fca85f0bdbabbe2
ortemx <46105878+ortemx@users.noreply.github.com>
2023-03-07 16:01:05 +0000

    update lab4

--- term6/IB/lab4_improved.py
+++ term6/IB/lab4_improved.py
@@ -19,13 +19,12 @@ class Message:
     def __init__(self, text: str):
         public_key = gen_public_key()
         private_key = gen_private_key(public_key)
-        e = public_key[0]
         n = public_key[1]
         d = private_key
         h = get_hash_of_text(text, n)
-        
+
         self.__text = text
-        self.__key = public_key        
+        self.__key = public_key
         self.__signature = pow(h, d, n)
 
     @property
@@ -33,18 +32,18 @@ class Message:
         """текст сообщения
         """
         return self.__text
-    
+
     @property
     def key(self):
         """Открытый ключ, состоящий из 2 частей:
         открытой экспоненты и модуля
         """
         return self.__key
-    
+
     @property
     def signature(self):
         return self.__signature
-    
+
     def print(self) -> None:
         """выводит информацию о сообщении
         """
@@ -63,19 +62,19 @@ def is_prime(num: int) -> bool:
     for i in range(2, sqr + 1):
         if num % i == 0:
             return False
-    return True 
+    return True
 
-     
+
 def gen_public_key() -> tuple:
     """функция генерации открытого ключа
 
     Returns:
         tuple: e, n, значении функции Эйлера в n
     """
-    
+
     p: int = 0
     q: int = 0
-    
+
     min_p: int = randint(10000, 20000)
     while True:
         if is_prime(min_p):
@@ -84,42 +83,46 @@ def gen_public_key() -> tuple:
             break
         else:
             min_p += 1
-            
+
     min_q: int = randint(20000, 30000)
     while True:
         if is_prime(min_q):
             q = min_q
             break
         else:
-            min_q += 1    
-    
+            min_q += 1
+
     n = p * q
     fi_n: int = (p - 1) * (q - 1)
-    
+
     e: int = 0
     for i in range (fi_n - 1, int(math.sqrt(fi_n)), -1):
         if math.gcd(i, fi_n) == 1:
             e = i
             break
-    
+
     return (e, n, fi_n)
 
 
 def gen_private_key(public_key: tuple) -> int:
+    """Генерирует закрытый ключ
+
+    Args:
+        public_key (tuple): открытый ключ
+    """
     e = public_key[0]
-    n = public_key[1]
     fi_n = public_key[2]
-    
+
     set_of_d = []
     d = 199 * e
 
-    while set_of_d.__len__() < 20:
+    while len(set_of_d) < 20:
         if d * e % fi_n == 1:
             set_of_d.append(d)
             d += e
         d += 1
 
-    return d
+    return choices(set_of_d)
 
 
 def get_hash_of_text(text: str, n: int) -> int:
@@ -130,8 +133,6 @@ def get_hash_of_text(text: str, n: int) -> int:
         text (str): входное сообщение
         n (int): модуль, по которому высчитывается значение
         функции (должен быть больше кода любого символа алфаваита)
-    Returns:
-        int: 
     """
 
     hash_code: int = 0
@@ -163,7 +164,7 @@ def validation_message(message: Message) -> bool:
     return hash_of_text == hash_of_key
 
 
-txt = "azbuka"
-msg = Message(txt)
+TXT = "azbuka"
+msg = Message(TXT)
 
 print("отправитель подтвержден" if validation_message(msg) else "отправитель не подтвержден")
