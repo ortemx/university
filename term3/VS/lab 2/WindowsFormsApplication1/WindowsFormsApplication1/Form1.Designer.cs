c650e104cb0ec6e846c2747eeeba5939609751b0
ortemx <printerq@yandex.ru>
2023-04-29 20:26:41 +0300

    удалил ненужные программы

--- term3/VS/lab 2/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ term3/VS/lab 2/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -40,81 +40,67 @@
             // 
             // listBox1
             // 
-            this.listBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 20;
             this.listBox1.Location = new System.Drawing.Point(72, 58);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(459, 484);
+            this.listBox1.Size = new System.Drawing.Size(437, 368);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // textBox1
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
-            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox1.ForeColor = System.Drawing.SystemColors.WindowText;
             this.textBox1.Location = new System.Drawing.Point(811, 120);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(141, 30);
+            this.textBox1.Size = new System.Drawing.Size(141, 20);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox2.Location = new System.Drawing.Point(811, 156);
+            this.textBox2.Location = new System.Drawing.Point(811, 171);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(141, 30);
+            this.textBox2.Size = new System.Drawing.Size(141, 20);
             this.textBox2.TabIndex = 2;
             // 
             // textBox3
             // 
-            this.textBox3.BackColor = System.Drawing.SystemColors.Menu;
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.textBox3.Location = new System.Drawing.Point(811, 192);
+            this.textBox3.Location = new System.Drawing.Point(811, 215);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(141, 30);
+            this.textBox3.Size = new System.Drawing.Size(141, 20);
             this.textBox3.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(749, 123);
+            this.label1.Location = new System.Drawing.Point(712, 116);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 25);
+            this.label1.Size = new System.Drawing.Size(19, 13);
             this.label1.TabIndex = 4;
             this.label1.Text = "a=";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(749, 159);
+            this.label2.Location = new System.Drawing.Point(712, 174);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 25);
+            this.label2.Size = new System.Drawing.Size(19, 13);
             this.label2.TabIndex = 5;
             this.label2.Text = "b=";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(749, 192);
+            this.label3.Location = new System.Drawing.Point(712, 218);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 25);
+            this.label3.Size = new System.Drawing.Size(30, 13);
             this.label3.TabIndex = 6;
             this.label3.Text = "eps=";
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(717, 373);
+            this.button1.Location = new System.Drawing.Point(774, 395);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(235, 53);
+            this.button1.Size = new System.Drawing.Size(177, 80);
             this.button1.TabIndex = 7;
             this.button1.Text = "выполнить";
             this.button1.UseVisualStyleBackColor = true;
--- term3/VS/lab 2/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ term3/VS/lab 2/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -12,11 +12,15 @@ namespace WindowsFormsApplication1
 {
     public partial class Form1 : Form
     {
-        double x, y, eps, a, sum, F, aa, bb;
+        double x, y, eps, a, sum, F;
         int n;
-        string s1, s2;
-        public string oldWxStr;
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        string s1, s2;
         public Form1()
         {
             InitializeComponent();
@@ -24,93 +28,17 @@ namespace WindowsFormsApplication1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-        }
-
-        private void textBox1_Enter(object sender, EventArgs e)
-        {
-            oldWxStr = textBox1.Text;
 
         }
 
-        private void textBox2_Enter(object sender, EventArgs e)
-        {
-            oldWxStr = textBox2.Text;
-        }
-        private void textBox3_Enter(object sender, EventArgs e)
-        {
-            oldWxStr = textBox3.Text;
-        }
-        private void textBox1_Leave(object sender, EventArgs e)
-        {
-            try
-            {
-                //Какие - либо действия...
-                aa = Convert.ToDouble(textBox1.Text);
-            }
-            catch (Exception ee)
-            {
-                //Возникла исключительная ситуация...
-                MessageBox.Show("Недопустимое числовое значение...");
-                textBox1.Text = oldWxStr;
-
-                textBox1.Focus();
-            }
-            finally
-            {
-                //Завершающие действия обработчика...
-            }
-        }
-        private void textBox2_Leave(object sender, EventArgs e)
-        {
-            try
-            {
-                //Какие - либо действия...
-                bb = Convert.ToDouble(textBox2.Text);
-            }
-            catch (Exception ee)
-            {
-                //Возникла исключительная ситуация...
-                MessageBox.Show("Недопустимое числовое значение...");
-                textBox2.Text = oldWxStr;
-
-                textBox1.Focus();
-            }
-            finally
-            {
-                //Завершающие действия обработчика...
-            }
-        }
-        private void textBox3_Leave(object sender, EventArgs e)
-        {
-            try
-            {
-                //Какие - либо действия...
-                eps = Convert.ToDouble(textBox3.Text);
-            }
-            catch (Exception ee)
-            {
-                //Возникла исключительная ситуация...
-                MessageBox.Show("Недопустимое числовое значение...");
-                textBox3.Text = oldWxStr;
-
-                textBox1.Focus();
-            }
-            finally
-            {
-                //Завершающие действия обработчика...
-            }
-        }
         private void button1_Click(object sender, EventArgs e)
         {
-            aa = Convert.ToDouble(textBox1.Text);
-            bb = Convert.ToDouble(textBox2.Text);
-            eps = Convert.ToDouble(textBox2.Text);
             eps = 0.001;
-            listBox1.Items.Clear();
             for (x = 0.1; x <= 1; x += 0.05)
             {
                 a = x;
@@ -125,8 +53,8 @@ namespace WindowsFormsApplication1
                 } while (Math.Abs(a) >= eps);
                 F = (Math.Exp(x) - Math.Exp(-x)) / 2;
                 s1 = x.ToString() + "     "; s1 = s1.Substring(0, 6);
-                s2 = sum.ToString() + "     "; s2 = s2.Substring(0, 6);                
-                listBox1.Items.Add(s1  + "\t" + " " + s2 + " " + "\t" + F);
+                s2 = sum.ToString() + "     "; s2 = s2.Substring(0, 6);
+                listBox1.Items.Add(s1 = "\t" + " " + s2 + " " + "\t" + F);
             }
         }
     }
