b1ef9a8a484cb265e77f5b0a7a6cb773b9a7abb2
ortemx <printerq@yandex.ru>
2023-09-26 22:35:13 +0300

    смена типа документа

--- term7/Pat/lab2/lab2.py
+++ term7/Pat/lab2/lab2.py
@@ -3,24 +3,159 @@
 '''
 
 from abc import ABC, abstractmethod
-from term7.Pat.lab2.Factories.abstract_interface_factory import InterfaceFactory
 
+class InterfaceFactory(ABC):
+    '''
+    Фабрика интерфейса
+    '''
+    @abstractmethod
+    def create_text(self):
+        '''
+        Создание текста
+        '''
 
+    @abstractmethod
+    def create_image(self):
+        '''
+        Создание изображения
+        '''
 
+    @abstractmethod
+    def create_help(self):
+        '''
+        Создание справки
+        '''
 
+class Text(ABC):
+    '''
+    Абстрактный класс текста интерфейса
+    '''
+    @property
+    @abstractmethod
+    def text(self):
+        pass
 
+class Image(ABC):
+    '''
+    Абстрактный класс изображения интерфейса
+    '''
+    @property
+    @abstractmethod
+    def image(self):
+        pass
 
+class Help(ABC):
+    '''
+    Абстрактный класс справки интерфейса
+    '''
+    @property
+    @abstractmethod
+    def help(self):
+        pass
 
+class RussianText(Text):
+    '''
+    Русский текст
+    '''
+    def __init__(self):
+        self._text = "Русский текст"
+    @property
+    def text(self):
+        return self._text
 
+class RussianImage(Image):
+    '''
+    Изобажение русского интерфейса
+    '''
+    def __init__(self):
+        self._image = "Изобажение русского интерфейса"
+    @property
+    def image(self):
+        return self._image
 
+class RussianHelp(Help):
+    '''
+    Справка на русском языке
+    '''
+    def __init__(self):
+        self._help = "Справка русского интерфейса"
+    @property
+    def help(self):
+        return self._help
 
+class EnglishText(Text):
+    '''
+    Английский текст
+    '''
+    def __init__(self):
+        self._text = "Английский текст"
+    @property
+    def text(self):
+        return self._text
 
+class EnglishImage(Image):
+    '''
+    Изобажение английского интерфейса
+    '''
+    def __init__(self):
+        self._image = "Изобажение английского интерфейса"
+    @property
+    def image(self):
+        return self._image
 
+class EnglishHelp(Help):
+    '''
+    Справка на английском языке
+    '''
+    def __init__(self):
+        self._help = "Справка английского интерфейса"
+    @property
+    def help(self):
+        return self._help
 
+class FrenchText(Text):
+    '''
+    Французский текст
+    '''
+    def __init__(self):
+        self._text = "Французский текст"
+    @property
+    def text(self):
+        return self._text
 
+class FrenchImage(Image):
+    '''
+    Изобажение французского интерфейса
+    '''
+    def __init__(self):
+        self._image = "Изобажение французского интерфейса"
+    @property
+    def image(self):
+        return self._image
 
+class FrenchHelp(Help):
+    '''
+    Справка на французском языке
+    '''
+    def __init__(self):
+        self._help = "Справка французского интерфейса"
+    @property
+    def help(self):
+        return self._help
 
+class RussianInterfaceFactory(InterfaceFactory):
+    '''
+    Фабрика русского интерфейса
+    '''
 
+    def create_text(self):
+        return RussianText()
+
+    def create_image(self):
+        return RussianImage()
+
+    def create_help(self):
+        return RussianHelp()
 
 
 class EnglishInterfaceFactory(InterfaceFactory):
