22917163b01392ef170ee7785d74cbd7e5ed4162
ortemx <printerq@yandex.ru>
2023-10-02 23:12:13 +0300

    lab 2 done

--- term7/Pat/lab2/lab2.py
+++ term7/Pat/lab2/lab2.py
@@ -4,184 +4,241 @@
 
 from abc import ABC, abstractmethod
 
-class InterfaceFactory(ABC):
-    '''
-    Фабрика интерфейса
-    '''
-    @abstractmethod
-    def create_text(self):
-        '''
-        Создание текста
-        '''
 
-    @abstractmethod
-    def create_image(self):
-        '''
-        Создание изображения
-        '''
-
-    @abstractmethod
-    def create_help(self):
-        '''
-        Создание справки
-        '''
-
+# Базовые классы интерфейса
 class Text(ABC):
     '''
     Абстрактный класс текста интерфейса
     '''
-    @property
+    def __init__(self, text: str):
+        self._text = text
+
     @abstractmethod
-    def text(self):
+    def desc(self):
         pass
+
 
 class Image(ABC):
     '''
     Абстрактный класс изображения интерфейса
     '''
-    @property
+    def __init__(self, image: str):
+        self._image = image
+
     @abstractmethod
-    def image(self):
+    def desc(self):
         pass
+
 
 class Help(ABC):
     '''
     Абстрактный класс справки интерфейса
     '''
-    @property
+    def __init__(self, help: str):
+        self._help = help
     @abstractmethod
-    def help(self):
+    def desc(self):
         pass
 
+
+# базовый класс фабрики интерфейса
+class InterfaceFactory(ABC):
+    '''
+    Фабрика интерфейса
+    '''
+    @abstractmethod
+    def create_text(self) -> Text:
+        '''
+        Создание текста
+        '''
+
+    @abstractmethod
+    def create_image(self) -> Image:
+        '''
+        Создание изображения
+        '''
+
+    @abstractmethod
+    def create_help(self) -> Help:
+        '''
+        Создание справки
+        '''
+
+
+# класс Клиента
+class Client:
+    '''
+    Клиент
+    '''
+    def __init__(self, factory: InterfaceFactory):
+        self._factory = factory
+        self.text = None
+        self.image = None
+        self.help = None
+
+    def run(self):
+        text = self._factory.create_text()
+        image = self._factory.create_image()
+        help = self._factory.create_help()
+        text.desc()
+        image.desc()
+        help.desc()
+  
+
+
+# производные классы русского интерфейса
 class RussianText(Text):
     '''
     Русский текст
     '''
     def __init__(self):
-        self._text = "Русский текст"
-    @property
-    def text(self):
-        return self._text
+        super().__init__("Русский текст")
+        
+    def desc(self):
+        print("Создание русского текста")
+
 
 class RussianImage(Image):
     '''
     Изобажение русского интерфейса
     '''
     def __init__(self):
-        self._image = "Изобажение русского интерфейса"
-    @property
-    def image(self):
-        return self._image
+        super().__init__("Изобажение русского интерфейса")
+
+    def desc(self):
+        print("Создание изображения русского интерфейса")
+
 
 class RussianHelp(Help):
     '''
     Справка на русском языке
     '''
     def __init__(self):
-        self._help = "Справка русского интерфейса"
-    @property
-    def help(self):
-        return self._help
+        super().__init__("Справка русского интерфейса")
 
+    def desc(self):
+        print("Создание справки русского интерфейса")
+
+
+# производные классы английского интерфейса
 class EnglishText(Text):
     '''
     Английский текст
     '''
     def __init__(self):
-        self._text = "Английский текст"
-    @property
-    def text(self):
-        return self._text
+        super().__init__("Английский текст")
+
+    def desc(self):
+        print("Создание английского текста")
+
 
 class EnglishImage(Image):
     '''
     Изобажение английского интерфейса
     '''
     def __init__(self):
-        self._image = "Изобажение английского интерфейса"
-    @property
-    def image(self):
-        return self._image
+        super().__init__("Изобажение английского интерфейса")
+    
+    def desc(self):
+        print("Создание изображения английского интерфейса")
+
 
 class EnglishHelp(Help):
     '''
     Справка на английском языке
     '''
     def __init__(self):
-        self._help = "Справка английского интерфейса"
-    @property
-    def help(self):
-        return self._help
+        super().__init__("Справка английского интерфейса")
+    def desc(self):
+        print("Создание справки английского интерфейса")
 
+
+# производные классы французского интерфейса
 class FrenchText(Text):
     '''
     Французский текст
     '''
     def __init__(self):
-        self._text = "Французский текст"
-    @property
-    def text(self):
-        return self._text
+        super().__init__("Французский текст")
+    def desc(self):
+        print("Создание французского текста")
+
 
 class FrenchImage(Image):
     '''
     Изобажение французского интерфейса
     '''
     def __init__(self):
-        self._image = "Изобажение французского интерфейса"
-    @property
-    def image(self):
-        return self._image
+        super().__init__("Изобажение французского интерфейса")
+
+    def desc(self):
+        print("Создание изображения французского интерфейса")
+
 
 class FrenchHelp(Help):
     '''
     Справка на французском языке
     '''
     def __init__(self):
-        self._help = "Справка французского интерфейса"
-    @property
-    def help(self):
-        return self._help
+        super().__init__("Справка французского интерфейса")
+    def desc(self):
+        print("Создание справки французского интерфейса")
 
+
+# производный класс фабрики русского интерфейса
 class RussianInterfaceFactory(InterfaceFactory):
     '''
     Фабрика русского интерфейса
     '''
 
-    def create_text(self):
+    def create_text(self) -> Text:
         return RussianText()
 
-    def create_image(self):
+    def create_image(self) -> Image:
         return RussianImage()
 
-    def create_help(self):
+    def create_help(self) -> Help:
         return RussianHelp()
 
 
+# производный класс фабрики английского интерфейса
 class EnglishInterfaceFactory(InterfaceFactory):
     '''
     Фабрика английского интерфейса
     '''
-    def create_text(self):
+    def create_text(self) -> Text:
         return EnglishText()
 
-    def create_image(self):
+    def create_image(self) -> Image:
         return EnglishImage()
 
-    def create_help(self):
+    def create_help(self) -> Help:
         return EnglishHelp()
 
+
+# производный класс фабрики французского интерфейса
 class FrenchInterfaceFactory(InterfaceFactory):
     '''
     Фабрика французского интерфейса
     '''
-    def create_text(self):
+    def create_text(self) -> Text:
         return FrenchText()
 
-    def create_image(self):
+    def create_image(self) -> Image:
         return FrenchImage()
 
-    def create_help(self):
+    def create_help(self) -> Help:
         return FrenchHelp()
 
-concrete_interface = RussianInterfaceFactory()
+def chose_factory(language: str) -> InterfaceFactory:
+    factory_dict = {
+        "Ru": RussianInterfaceFactory,
+        "En": EnglishInterfaceFactory,
+        "Fr": FrenchInterfaceFactory
+    }
+    return factory_dict[language]()
+
+
+interface = chose_factory("Ru")
+client = Client(interface)
+client.run()
