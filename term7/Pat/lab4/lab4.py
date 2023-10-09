e6bfd8fbcf1d66e4049cfab32e757ecd2de976ff
ortemx <printerq@yandex.ru>
2023-10-09 23:24:11 +0300

    add lab 4

--- term7/Pat/lab4/lab4.py
+++ term7/Pat/lab4/lab4.py
@@ -17,35 +17,63 @@
 и корректной работе адаптера.
 
 '''
-from abc import ABC, abstractmethod
 
-class LengthConverter(ABC):
-    @abstractmethod
-    def convert(self, length: float) -> float:
+
+class LengthConverter:
+    def convert_meters_to_feet(self, meters):
         pass
-  
-  
-class MeterConverter:
-    def convert_to_meter(self, length: float) -> float:
-        return length
 
-    def convert_to_feet(self, length: float) -> float:
-        return length * 3.28084 * 3.28084
+    def convert_feet_to_meters(self, feet):
+        pass
 
 
-class MeterToFeetAdapter(LengthConverter):
-    def __init__(self, meter_converter: MeterConverter):
-        self.meter_converter = meter_converter
 
-    def convert(self, length: float) -> float:
-        return self.meter_converter.convert_to_feet(length)
-    
+class MetricLength:
+    def __init__(self):
+        pass
+        # self.length = length
+
+    def get_length(self):
+        return self.length
+
+    def set_length(self, length):
+        self.length = length
 
 
-meter_converter = MeterConverter()
-adapter = MeterToFeetAdapter(meter_converter)
+class MetricToImperialAdapter(LengthConverter):
+    def __init__(self, metric_length):
+        self.metric_length = metric_length
 
-area_in_square_meter = 100
-area_in_square_feet = adapter.convert(area_in_square_meter)
+    def convert_meters_to_feet(self, meters):
+        self.metric_length.set_length(meters)
+        return self.metric_length.get_length() * 3.28084
 
-print(f"{area_in_square_meter} square meters = {area_in_square_feet} square feet")
+    def convert_feet_to_meters(self, feet):
+        self.metric_length.set_length(feet / 3.28084)
+        return self.metric_length.get_length()
+
+
+class LengthConversionApp:
+    def __init__(self, converter):
+        self.converter = converter
+
+    def convert_length(self, length, from_unit, to_unit):
+        if from_unit == "meters" and to_unit == "feet":
+            result = self.converter.convert_meters_to_feet(length)
+            return f"{length} meters is equal to {result} feet."
+        elif from_unit == "feet" and to_unit == "meters":
+            result = self.converter.convert_feet_to_meters(length)
+            return f"{length} feet is equal to {result} meters."
+        else:
+            return "Unsupported conversion."
+
+
+# metric_length = (10)  # создаем экземпляр адаптируемого класса
+adapter = MetricToImperialAdapter(MetricLength())  # создаем экземпляр адаптера
+app = LengthConversionApp(adapter)  # создаем экземпляр клиентского приложения
+
+result = app.convert_length(10, "meters", "feet")  # конвертируем 10 метров в футы
+print(result)  # выводит "10 meters is equal to 32.8084 feet."
+
+result = app.convert_length(20, "feet", "meters")  # конвертируем 20 футов в метры
+print(result)  # выводит "20 feet is equal to 6.096 meters."
