5caf000458411d729045e988b446f23d951aae5c
Konyshev Artem <46105878+ortemx@users.noreply.github.com>
2023-11-09 19:29:24 +0000

    исправил ошибки

--- term7/Pat/lab3/Factory/Factories/ReportFactory.cs
+++ term7/Pat/lab3/Factory/Factories/ReportFactory.cs
@@ -12,8 +12,9 @@
             string author = Console.ReadLine();
             Console.WriteLine("Type content");
             string content = Console.ReadLine();
-            Console.WriteLine("\nType date in format dd.MM.yyyy HH:mm:ss");
-            DateTime date = DateTime.Parse(Console.ReadLine());
+            var iso8601String = "yyyyMMddTHH:mm:ssZ";
+            Console.WriteLine("\nType date in format yyyyMMddTHH:mm:ssZ");
+            DateTime date = DateTime.ParseExact(Console.ReadLine(), iso8601String, System.Globalization.CultureInfo.InvariantCulture);
             return new Report(title, author, content, date);
         }
     }
