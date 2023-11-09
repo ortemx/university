875cf97ec37c2a9eaf54b938fd3e9917bd4667c8
ortemx <printerq@yandex.ru>
2023-11-09 22:19:56 +0300

    что я наделал

--- term7/VM/lab7/Program.cs
+++ term7/VM/lab7/Program.cs
@@ -1,96 +1,39 @@
-﻿using static System.Math;
+﻿using System;
 
 class Program
 {
-    delegate double Functions(double x);
-
-    static double MidpointRectangleRule(Functions function, double a, double b)
+    static double Function(double x)
     {
-        return (b - a) * function((a + b) / 2);
+        return Math.Sin(1/(x*x));
     }
 
-    static double TrapezoidalRule(Functions function, double a, double b)
+    static double MidpointRectangleRule(Func<double, double> function, double a, double b)
     {
-        return (b - a) * ((function(a) + function(b)) / 2);
+        double midpoint = (a + b) / 2;
+        double result = (b - a) * function(midpoint);
+        return result;
     }
 
-    static double SimpsonRule(Functions function, double a, double b)
+    static double TrapezoidalRule(Func<double, double> function, double a, double b, int n)
     {
-        return (b - a) * (function(a) + 4 * function((a + b) / 2) + function(b)) / 6;
-    }
-    struct Limits
-    {
-        public double a;
-        public double b;
-    }
+        double h = (b - a) / n;
+        double sum = (function(a) + function(b)) / 2;
 
-    static void Main()
-    {
-        List<Functions> functions = new()
+        for (int i = 1; i < n; i++)
         {
-            (x) => Sin(1 / (x * x)),
-            (x) => x * Sin(1 / (x * x * x)),
-            (x) => Exp(x) * Cos(1 / (x * x * x)),
-            (x) => Exp(x) / x * Cos(1 / (x * x * x)),
-            (x) => Pow(2,x) * Log(x)
-        };
-
-        List<Limits> limits = new() {
-            new Limits { a = 1, b = 2 },
-            new Limits { a = 1, b = 2 },
-            new Limits { a = 1, b = 2 },
-            new Limits { a = 2, b = 4 },
-            new Limits { a = 3, b = 4 },
-        };
-
-        List<double[]> values = new()
-        {
-            new double[] { 0, 0 },
-            new double[] { 0, 0 },
-            new double[] { 0, 0 },
-        };
-
-        int n = 1000;
-
-        for (int i = 0; i < functions.Count; i++)
-        {
-
-            double h = (limits[i].b - limits[i].a) / n;
-
-            for (double x = limits[i].a; x < limits[i].b; x += h)
-            {
-                values[0][0] += functions[i](x + h / 2);
-            }
-            values[0][0] *= h;
-
-
-            values[1][0] = 0.5 * (functions[i](limits[i].a) + functions[i](limits[i].b));
-            for (double x = limits[i].a + h; x <= limits[i].b - h; x += h)
-            {
-                values[1][0] += functions[i](x);
-            }
-            values[1][0] *= h;
-
-
-            values[2][0] = functions[i](limits[i].a) + functions[i](limits[i].b);
-            for (double m = 1; m < n; m++)
-            {
-                if (m % 2 == 0)
-                {
-                    values[2][0] += 2 * functions[i](limits[i].a + m * h);
-                }
-                else
-                {
-                    values[2][0] += 4 * functions[i](limits[i].a + m * h);
-                }
-            }
-            values[2][0] *= h / 3;
-
-            Console.WriteLine(
-                $"{values[0][0],10:f4}" +
-                $"{values[1][0],10:f4}" +
-                $"{values[2][0],10:f4}"
-            );
+            double x = a + i * h;
+            sum += function(x);
         }
+
+        return h * sum;
+    }    
+
+    static void Main(string[] args)
+    {
+        double a = 1;
+        double b = 2;
+
+        double integral = MidpointRectangleRule(Function, a, b);
+        Console.WriteLine("Approximate integral: " + integral);
     }
 }
