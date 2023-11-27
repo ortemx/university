a5dc6eef9af47db2414181ec1feeb5ed72226c79
ortemx <printerq@yandex.ru>
2023-11-27 21:55:52 +0300

    lab 5 decorator

---
+++ term7/Pat/lab5/Decorator/Program.cs
@@ -0,0 +1,69 @@
+﻿Warrior warrior = new Warrior();
+Console.WriteLine(warrior);
+Armed halberdiers = new Armed(warrior);
+Console.WriteLine(halberdiers);
+
+abstract class GameCharacter
+{
+    public string WarriorType = string.Empty;
+    public int health;
+    public int damage;
+
+    public abstract override string ToString();
+}
+
+internal class Warrior : GameCharacter
+{
+    public Warrior()
+    {
+        WarriorType = "Warrior";
+        health = 100;
+        damage = 10;
+    }
+    public override string ToString()
+    {
+        return
+            $$"""
+            {{WarriorType}}
+                health: {{health}}
+                damage: {{damage}}
+            """;
+    }
+}
+
+abstract class Decorator : GameCharacter
+{
+    protected GameCharacter character;
+
+    public Decorator(GameCharacter character)
+    {
+        this.character = character;
+    }
+}
+
+internal class Armed : Decorator
+{
+    protected string weapon;
+    protected int armore;
+    public Armed(GameCharacter character) : base(character)
+    {
+        this.character = character;
+        weapon = "Halberd";
+        WarriorType = "Halberdiers";
+        health = character.health;
+        damage = character.damage + 100;
+        armore = 50;
+    }
+
+    public override string ToString()
+    {
+        return
+        $"""
+        {WarriorType}
+            health: {health}
+            damage: {damage}
+            weapon: {weapon}
+            armore: {armore}
+        """;
+    }
+}
