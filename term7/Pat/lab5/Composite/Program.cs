9b2719edefc9fb61b63cb42673bb0cbbaf230227
ortemx <printerq@yandex.ru>
2023-11-27 15:43:32 +0300

    add lab5 composite

---
+++ term7/Pat/lab5/Composite/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Console.WriteLine(Console.WindowWidth); 
+internal interface IText
+{
+    abstract public void Operation();
+    abstract public void Add(IText c);
+    abstract public void Remove(IText c);
+}
+
+internal class Word : IText
+{
+    public void Operation()
+    {
+        Console.WriteLine();
+    }
+    public void Add(IText c)
+    {
+        throw new InvalidOperationException();
+    }
+    public void Remove(IText c)
+    {
+        throw new InvalidOperationException();
+    }
+}
+
