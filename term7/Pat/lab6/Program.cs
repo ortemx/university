2f432d3df0b56f8bac348ac084e7a961ee11ba55
ortemx <printerq@yandex.ru>
2023-12-04 22:11:30 +0300

    add lab6

---
+++ term7/Pat/lab6/Program.cs
@@ -0,0 +1,83 @@
+﻿/*
+pattern chain of responsibility
+*/
+
+Article article = new Article("Technology", false);
+Handler editorial = new Editorial1(null);
+Handler editorial2 = new Editorial2(editorial);
+Handler editorial3 = new Editorial3(editorial2);
+
+editorial3.Publish(ref article);
+
+Console.WriteLine(article.readyForPublication);
+
+
+struct Article
+{
+    public string type;
+    public bool readyForPublication;
+    public Article(string type, bool readyForPublication)
+    {
+        this.type = type;
+        this.readyForPublication = readyForPublication;
+    }
+}
+abstract class Handler
+{
+    protected Handler? successor;
+    abstract public void Publish(ref Article article);
+}
+
+class Editorial1 : Handler
+{
+    public Editorial1(Handler? successor)
+    {
+        this.successor = successor;
+    }
+
+    public override void Publish(ref Article  article)
+    {
+        if (article.type == "Science" || article.type == "Technology")
+        {
+            article.readyForPublication = true;
+        } else if (successor != null) {
+            successor.Publish(ref article);
+        }
+    }
+}
+
+class Editorial2 : Handler
+{
+    public Editorial2(Handler successor)
+    {
+        this.successor = successor;
+    }
+
+    public override void Publish(ref Article  article)
+    {
+        if (article.type == "Business")
+        {
+            article.readyForPublication = true;
+        } else if(successor != null) {
+            successor.Publish(ref article);
+        }
+    }
+}
+
+class Editorial3 : Handler
+{
+    public Editorial3(Handler successor)
+    {
+        this.successor = successor;
+    }
+
+    public override void Publish(ref Article  article)
+    {
+        if (article.type == "Beauty")
+        {
+            article.readyForPublication = true;
+        } else if(successor != null) {
+            successor.Publish(ref article);
+        }
+    }
+}
